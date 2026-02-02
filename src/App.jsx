682c2304ddfff32f3929e9986fa0e50d3f8ef22f
Your Name <you@example.com>
2026-02-02 01:42:19 -0500

    [MB][register file added]

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/header'
-import Register from './pages/Register'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const route=createBrowserRouter([
+    {
+    path: "/Login",
+    element:<Login/>
+  },
+  {
+    path:"/Register",
+    element:<Register/>,
+  }
 
-  return (
-    <>
-      <Header/>
-      <Register/>
-    </>
-  )
+  ])
+
+  return <RouterProvider router={route}/>
+  
 }
 
 export default App
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,148 @@
-import React from "react";
+import "./Register.css";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 const Register = () => {
+  //state declaration section
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    phone: "",
+  });
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate()
+
+  //logic section
+
+  const validate = () => {
+    const newErrors = {};
+
+    if (!formData.name.trim()) {
+      newErrors.name = "Full name is  required.";
+    } else if (formData.name.length <= 3) {
+      newErrors.name = "Minimum 3 characters required.";
+    }
+    if (!formData.email.trim()) {
+      newErrors.email = "Email is required.";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+      newErrors.email = "invalid Email format.";
+    }
+    if (!formData.phone.trim()) {
+      newErrors.phone = "phone number is required";
+    } else if (!/^[0-9]{10}$/.test(formData.phone)) {
+      newErrors.phone = "Phone must be in  10 digit.";
+    }
+    if (!formData.password.trim()) {
+      newErrors.password = "password is required.";
+    } else if (formData.password.length < 6) {
+      newErrors.password = "Minimum 6 character required.";
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleInputChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    //e.target.name =e.target.value
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+    setErrors({
+    ...errors,
+    [e.target.name]:""
+  })
+
+  };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(validate()){
+      localStorage.setItem('authData',JSON.stringify(formData))
+      alert('Registration successfull...!')
+      navigate("/login")
+    }
+  };
+
+  //useEffect(()=>{
+  // console.log(formData)
+  //},[formData])
+
+  //design section
   return (
     <>
-    <div className="form-container">
-        <h1>This is Register.</h1>
+      <div className="form-container">
+        {/* Page Title */}
+        <h1 className="from-title"> REGISTER</h1>
+        <form onSubmit={handleSubmit}>
+          {/* Name Field */}
+          <div className="form-group">
+            <label htmlFor="name">Full Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              placeholder="Enter your full name"
+              onChange={handleInputChange}
+            />
+            {errors.name && <span className="error-msg">{errors.name}</span>}
+          </div>
 
-        </div>
+          {/* Email Field */}
+          <div className="form-group">
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              placeholder="Enter your email"
+              onChange={handleInputChange}
+            />
+            {errors.email && <span className="error-msg">{errors.email}</span>}
+          </div>
+
+          {/* Phone Number Field */}
+          <div className="form-group">
+            <label htmlFor="phone">Phone Number</label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              value={formData.phone}
+              placeholder="Enter your phone number"
+              onChange={handleInputChange}
+            />
+            {errors.phone && <span className="error-msg">{errors.phone}</span>}
+          </div>
+
+          {/* Password  Field */}
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              placeholder="Create a password"
+              onChange={handleInputChange}
+            />
+            {errors.password && <span className="error-msg">{errors.password}</span>}
+          </div>
+
+          {/* Submiit Button */}
+          <button type="submit" className="btn-primary">
+            Register
+          </button>
+        </form>
+
+        {/*Link to Register page*/}
+        <p ClassName="link-text">
+          Already have an account?<Link to="/Login">Login here</Link>
+        </p>
+      </div>
     </>
   );
 };
